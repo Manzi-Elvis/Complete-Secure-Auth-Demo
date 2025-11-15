20fe22a5a02dbde6b01e7b898378209507f84b1f
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-15 12:42:31 +0200

    Added server contents

---
+++ Server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const app = express();
+const PORT = process.env.PORT || 5000;
+require('dotenv').config();
+
+// Connecting to MongoDB
+connectDB();
+app.use(express.json());
+
+// Defining routes
+app.use('/api/users', userRoutes);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}
+);
+
+module.exports = app;
