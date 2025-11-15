534eb8ecd71842ff789cbe84789eb576dc0edefa
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-15 14:35:22 +0200

    Added auth pages to the frontend

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Client/src/pages/SignIn.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+function SignIn() {
+  return <div>SignIn Page</div>;
+}
