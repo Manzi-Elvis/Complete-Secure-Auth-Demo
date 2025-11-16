4f2fef6c3d5f9804ba7508ac76b9bda0778ce896
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-16 16:24:37 +0200

    updated

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require("cors")
 const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ require('dotenv').config();
 connectDB();
 app.use(express.json());
 
+app.use(cors())
 // Defining routes
 app.use('/api/users', userRoutes);
 app.listen(PORT, () => {
