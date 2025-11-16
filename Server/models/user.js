c7637e7624083f9f8e93dcea6630823eaa81effa
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-16 16:43:45 +0200

    Password validation using restriction and regex

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,20 +1,27 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    name: {
+      name: {
             type: String,
             required: true
-    },
-    email: {
+      },
+      email: {
             type: String,
             required: true,
             unique: true,
             match: [/\S+@\S+\.\S+/, 'Please use a valid email address']
-    },
-
-    password: {
-            type: String,
-            required: true,
       },
 
-});
+      password: {
+            type: String,
+            required: [true, 'Password is required'],
+            minlength: [8, 'Password must be at least 8 characters long'],
+            validate: {
+                  validator: function(value) {
+                        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._-])[A-Za-z\d@$!%*?&._-]{8,}$/.test(value);
+                  },
+                  message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+            }
+      },
+
+},{ timestamps: true });
 module.exports = mongoose.model('User', userSchema);
