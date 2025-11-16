63350fc204c2b9fff200da2a839e7cde4f6cd27a
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-16 22:11:10 +0200

    Sign In page

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,4 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function SignIn() {
-  return <div>SignIn Page</div>;
+  return(
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+    <div className='bg-white p-3 rounded w-25'>
+      <h2>Sign In</h2>
+      <form action="">
+        <div className='mb-3'>
+          <label htmlFor="email">
+            <strong>Email</strong>
+          </label>
+          <input type="email" placeholder='Enter your email' autoComplete='off' name='email' className='form-control rounded-0'  onChange={(e) => setEmail(e.target.value)}  />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor="name">
+            <strong>Password</strong>
+          </label>
+          <input type="password" placeholder='Enter your password' name='password' className='form-control rounded-0'  onChange={(e) => setPassword(e.target.value)}  />
+        </div>
+        <button type='submit' className='btn btn-success w-100 rounded-0'>Sign In</button>
+      </form>
+      <p>Don't have an account?<Link to='/'>SignUp </Link></p>
+    </div>                    
+    </div>
+  )
 }
