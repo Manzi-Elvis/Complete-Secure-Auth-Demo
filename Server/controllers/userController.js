e4c8abb7041d4187187dbd049c8683aee6b86084
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-16 16:55:25 +0200

    CREATE a new user controller

---
+++ Server/controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs')
+
+//Creating a new user:
+exports.createUser = async (req, res) => {
+      try{
+            const { name, email, password } = req.body
+            if (!name || !email || !password ){
+                  return res.status(400).json({message: 'All fields are required'})
+            }
+            const existing = await UserActivation.findOne({email});
+            if(existing){
+                  return res.status(400).json({message: "Email already exists"});
+            }
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const user = await User.create({name, email, password: hashedPassword});
+            res.status(201).json(user)
+      }
+      catch(error){
+            res.status(500).json({message: error.message})
+      }
+};
