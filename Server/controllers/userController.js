d3bc338048796b13b72f38c0ad587931a3c855b8
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-16 21:11:59 +0200

    GET user by id

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -21,7 +21,7 @@ exports.createUser = async (req, res) => {
       }
 };
 
-// Get all users
+// Get all users:
 exports.getAllUsers = async(req, res) => {
       try{
             const users = await User.find();
@@ -31,3 +31,15 @@ exports.getAllUsers = async(req, res) => {
             res.status(500).json({message: error.message});
       }
 };
+
+// Get user by ID:
+exports.getUserById = async (req, res) => {
+      try{
+            const user = await User.findById(req.params.id);
+            if(!user) return res.status(404).json({message: 'User not found'})
+            res.json(user);
+      }
+      catch (error) {
+            res.status(500).json({ message: error.message });
+      }
+}
