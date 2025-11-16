be4b3faf29bd4e2286bb00398f065b8f9cc2979b
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-16 16:58:54 +0200

    GET all users

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -20,3 +20,14 @@ exports.createUser = async (req, res) => {
             res.status(500).json({message: error.message})
       }
 };
+
+// Get all users
+exports.getAllUsers = async(req, res) => {
+      try{
+            const users = await User.find();
+            res.json(users);
+      }
+      catch(error){
+            res.status(500).json({message: error.message});
+      }
+};
