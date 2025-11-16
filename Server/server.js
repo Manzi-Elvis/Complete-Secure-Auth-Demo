b530d0d5dc8dc27beb15273f582078d5cb22f1a6
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-16 16:30:20 +0200

    ...

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require("cors")
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRoutes.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
