0f1c23ba339c88f88e115d13c0200d511ddace98
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-16 21:40:51 +0200

    CREATE user

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
+router.post('/', userController.createUser);
 
 module.exports = router;
