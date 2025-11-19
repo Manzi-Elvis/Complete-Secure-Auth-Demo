d99a4993015d57927c43daf535fe37cd3d8d9e8c
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-19 21:01:16 +0200

    useNavigate

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export default function SignIn() {
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
